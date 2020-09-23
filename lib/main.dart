83777da6e5316015b67a67d1756313f582420d90
AmbujaAK <itsambuja@gmail.com>
2020-09-23 17:18:55 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Bloc'),
+      ),
+      body: Container(
+        child: Text('login'),
+      ),
+    );
+  }
+}
